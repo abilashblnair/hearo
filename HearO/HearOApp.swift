e1eb6f0953283f70b3701c77097820f3ae29bd99
Abilash Balasubramanian <abalasubramanian@diligent.com>
2025-08-12 20:04:09 +0530

    Initial Commit

---
+++ HearO/HearOApp.swift
@@ -0,0 +1,17 @@
+//
+//  HearOApp.swift
+//  HearO
+//
+//  Created by Abilash Balasubramanian on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct HearOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
